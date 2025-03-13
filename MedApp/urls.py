381bbd4d4cf82a11ab8a519631e3352e28b884d4
lu-ko-ri-to <michaelkitangasi@gmail.com>
2025-03-13 08:26:58 +0300

    initial commit

--- MedApp/urls.py
+++ MedApp/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from MedApp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-  path('', views.index, name='index'),
+  path('index/', views.index, name='index'),
   path('starter/',views.starter, name='starter'),
   path('about/', views.about, name='about'),
   path('service/', views.service, name='service'),
@@ -16,5 +16,13 @@ urlpatterns = [
   path('show/', views.show, name='show'),
   path('delete/<int:id>', views.delete, ),
   path('edit/<int:id>', views.edit, name='edit'),
+  path('', views.register, name='register'),
+  path('login/', views.login_view, name='login'),
+  #Mpesa API
+  path('pay/', views.pay, name='pay'),
+  path('stk/', views.stk, name='stk'),
+  path('token/', views.token, name='token'),
+  path('transactions/', views.transactions_list, name='transactions'),
+
 
 ]
